29f16907f4b643a8960bc3149c4b43d091ee4e7e
Michał Ciężki <m.ciezky97@gmail.com>
2020-10-01 19:51:51 +0200

    Updates

---
+++ precache-manifest.c775ac48a7113f7002d1a59cc71eb71b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c3f36195ee1ea0c2795",
+    "url": "/react-simple-spa/static/css/main.65e01588.chunk.css"
+  },
+  {
+    "revision": "5c3f36195ee1ea0c2795",
+    "url": "/react-simple-spa/static/js/main.d53d28cc.chunk.js"
+  },
+  {
+    "revision": "276bd999ae082a982781",
+    "url": "/react-simple-spa/static/js/runtime~main.e4de5cf1.js"
+  },
+  {
+    "revision": "23487973340609ec91d2",
+    "url": "/react-simple-spa/static/js/2.88b550cb.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-simple-spa/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-simple-spa/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-simple-spa/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "b8d64f356a7520cd443fbcb050cf87cc",
+    "url": "/react-simple-spa/index.html"
+  }
+];
